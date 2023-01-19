df09ac65dbc5fbe24691ae02b449c414611db8dc
tomiban <81537786+tomiban@users.noreply.github.com>
2023-01-19 12:29:36 -0300

    correcion de sintaxis componente item list conteiner

--- src/components/views/cardProducts/itemListContainer/ItemListContainer.jsx
+++ src/components/views/cardProducts/itemListContainer/ItemListContainer.jsx
@@ -18,14 +18,13 @@ export const ItemListContainer = () => {
 	const fetchData = async () => {
 		const data = await fetch(categoryId ? `https://fakestoreapi.com/products/category/${categoryId}`: 'https://fakestoreapi.com/products/');
 		const productos = await data.json();
-		setData(productos);
+		setData(<ItemList data={productos}></ItemList>);
 	};
 
 
 	return (
 		<div className="cards">
-			{' '}
-			<ItemList data={data} />
+			{data}
 		</div>
 	);
 };
