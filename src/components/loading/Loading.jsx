b834d9801dc6e3aa06d97be9bd82a420f632fbaa
tomiban <81537786+tomiban@users.noreply.github.com>
2023-02-07 11:33:06 -0300

    fix loading height

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -4,7 +4,9 @@ import Spinner from 'react-bootstrap/Spinner';
 const Loading = () => {
     return (
         
-            <div className="position-absolute top-100 start-50 translate-middle me-5 mb-5">
+        <div className="d-flex align-items-center justify-content-center me-5 mb-5" style={{
+            height: "25vw"
+        }}>
             <Spinner animation="border" variant="primary" role="status">
             <span className="visually-hidden">Loading...</span>
         </Spinner>
